c29322bb1fac6c1310b317310abb3036a8a60baf
Miguel Cano <miguel.cano@fitchsolutions.com>
2019-07-16 11:06:45 -0500

    added simple exercises for team to complete

--- nodejest/example1/__tests__/jestbasics.spec.js
+++ nodejest/example1/__tests__/jestbasics.spec.js
@@ -1,13 +1,13 @@
-const { multiply, add } = require('../jestbasics.js');
+const { multiply, add, getInitials } = require('../jestbasics.js');
 describe('jest basics',  () => {
   it('multiply function should return result of two multiplied arguments', () => {
     expect.hasAssertions();
 
     const multiplied = multiply(2, 3);
+    expect(multiplied).toBe(6); 
     expect(multiplied).toEqual(6);
     expect(multiplied).not.toEqual(5);
 
-    expect(multiplied).toBe(6); 
   });
 
   it('multiply function returns a number', () => {
@@ -16,5 +16,26 @@ describe('jest basics',  () => {
     expect(typeof multiplied).not.toBe('boolean');
   });
 
+  /**
+   * Write two expect statements.
+   * - One should consider when two arguments are passed. 
+   * - Second should either consider results for no arguments or one
+   * - If you have time, think of additional test opportunities  
+   */
   it.todo('add'); // also it.skip for broken tests jest v.24+
+
+
+  /**
+   * Write three expect statements. 
+   * - One test should be a success path 
+   * - the second should be a failure path
+   * - Add your own third test. Explore matcher options. 
+   * 
+   * Hints: 
+   * - success matcher options: .toBe() .toMatch() 
+   * - failure matcher options:  .toBeNull(), .toBeFalsy() 
+   */
+
+  it.todo('getInitial returns capitlazed initials or null');
+
 });
--- nodejest/example1/jestbasics.js
+++ nodejest/example1/jestbasics.js
@@ -3,12 +3,21 @@ export function multiply(a, b) {
   return a * b;
 }
 
-export function add(a, b) {
+export function add(a, b = 0) {
+  
+  if (!a) {
+    return 'Unable to add with missing arguments';
+  }
+  
   return a + b;
 }
 
+export function getInitials(firstName, lastName) {
+  return firstName && lastName && firstName.length > 0 && lastName.length > 0 ? (firstName[0] + lastName[0]).toUpperCase() : null;
+};
 
 module.export = {
   multiply,
-  add
+  add,
+  getInitials
 }
